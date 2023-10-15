d6991bb04ce03a783f694ceed917ba63c86f013f
Faezeh <fmadadi69@gmail.com>
2023-10-15 15:54:33 +0330

    Trying to add a button to admin page to retrieve data from BAMA.ir and insert into database tables.

--- CarFinder/CarFinder/settings.py
+++ CarFinder/CarFinder/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
@@ -27,10 +26,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
+    'CarRecomender.apps.CarrecomenderConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -69,17 +68,17 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'CarFinder.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            "read_default_file": str(BASE_DIR / 'db_config.cfg')
+        },
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -99,18 +98,16 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Iran'
 
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
--- CarFinder/CarRecomender/models.py
+++ CarFinder/CarRecomender/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
+
 # Create your models here.
+
+class ScrapingReport(models.Model):
+    report_date = models.DateTimeField()
+    counts = models.IntegerField(default=0)
+    last_retrieve_car = models.IntegerField()
+
+
+class Cars(models.Model):
+    scraping_report = models.ForeignKey(ScrapingReport, on_delete=models.CASCADE)
+    make = models.CharField(max_length=100)
+    mileage = models.CharField(max_length=100)
+    condition = models.CharField(max_length=100)
+    location = models.CharField(max_length=250)
+    price = models.CharField(max_length=100)
